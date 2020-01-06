835da0475853962fff53dec2afad0046a82a7aca
RobHarveyOTB <robert.harvey@onthebeach.co.uk>
2020-01-06 10:57:25 +0000

    First commit

---
+++ Api/Program.cs
@@ -0,0 +1,55 @@
+using Api.Database;
+using GraphQLStub;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = CreateWebHostBuilder(args).Build();
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                var authorDbEntry = context.Authors.Add(
+                  new Author
+                  {
+                      Name = "First Author",
+                  }
+                );
+
+                context.SaveChanges();
+
+                context.Books.AddRange(
+                  new Book
+                  {
+                      Id = "0",
+                      Name = "First Book",
+                      Published = true,
+                      AuthorId = authorDbEntry.Entity.Id,
+                      Genre = "Mystery"
+                  },
+                  new Book
+                  {
+                      Id = "1",
+                      Name = "Second Book",
+                      Published = true,
+                      AuthorId = authorDbEntry.Entity.Id,
+                      Genre = "Crime"
+                  }
+                );
+
+                context.SaveChanges();
+            }
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            var httpClient = new HttpClient();
+            var postUri = "https://localhost:5001/graphql";
+
+            var query = @"
+            {
+            author(id: 1) {
+                name
+            }
+            }";
+
+            var postData = new { Query = query };
+            var stringContent = new StringContent(JsonConvert.SerializeObject(postData), Encoding.UTF8, "application/json");
+
+            var res = await httpClient.PostAsync(postUri, stringContent);
+            if (res.IsSuccessStatusCode)
+            {
+                var content = await res.Content.ReadAsStringAsync();
+
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine($"Error occurred... Status code:{res.StatusCode}");
+            }
+        }
+    }
+}
